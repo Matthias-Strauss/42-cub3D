db3785bd0d563eeaae6f1cca95033e15f72f0ace
Kai Klockow <kklockow@1-B-2.42heilbronn.de>
2024-09-02 18:58:06 +0200

    homegoers push started with logic for convert_string_to_color

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kklockow <kklockow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 16:36:14 by kklockow          #+#    #+#             */
-/*   Updated: 2024/09/02 18:06:09 by kklockow         ###   ########.fr       */
+/*   Updated: 2024/09/02 18:57:07 by kklockow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,27 @@ int	next_comma(char *str, int start)
 	return (start);
 }
 
-int	convert_string_to_color(char *str)
+int	get_only_number(char *str, int start)
 {
 	char	*buffer;
+	int		number;
+
+	buffer = ft_substr(str, start, next_comma(str, start) - start);
+	number = ft_atoi(buffer);
+	printf("[%i]   [%s]\n", number, buffer);
+	free(buffer);
+	return (number);
+}
+
+int	convert_string_to_color(char *str)
+{
 	int		r;
 	int		g;
 	int		b;
 
-	buffer = ft_substr(str, 0, next_comma(str, 0));
-	r = ft_atoi(buffer);
-	printf("[%i]   [%s]\n", r, buffer);
-	free(buffer);
-	buffer = ft_substr(str, next_comma(str, 0) + 1, next_comma(str, next_comma(str, 0)));
-	g = ft_atoi(buffer);
-	printf("[%i]   [%s]\n", g, buffer);
-	free(buffer);
-	buffer = ft_substr(str, next_comma(str, next_comma(str, 0)) + 3, 4);
-	b = ft_atoi(buffer);
-	printf("[%i]   [%s]\n", b, buffer);
-	free(buffer);
+	r = get_only_number(str, 0);
+	g = get_only_number(str, next_comma(str, 0) + 1);
+	b = get_only_number(str, next_comma(str, next_comma(str, 0) + 1) + 1);
 
 	return (r << 24 | g << 16 | b << 8 | 100);
 }
