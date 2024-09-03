efa01cede2a78ee26caa08d50a6191706624b34b
Kai Klockow <kklockow@1-B-2.42heilbronn.de>
2024-09-03 13:42:22 +0200

    cleaned main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kklockow <kklockow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 16:36:14 by kklockow          #+#    #+#             */
-/*   Updated: 2024/09/03 12:01:48 by kklockow         ###   ########.fr       */
+/*   Updated: 2024/09/03 13:06:13 by kklockow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,9 @@
 int	main(int ac, char **av)
 {
 	t_main	*main;
-	mlx_t	*mlx;
-	mlx_image_t	*image;
 
 	main = init_structs();
 	parse_arguments(ac, av, main);
-
-
-	mlx = mlx_init(256, 256, "cub3d", false);
-	image = mlx_new_image(mlx, 256, 256);
-
-	for (int i = 0; i < 256; i++)
-		mlx_put_pixel(image, i, i, main->map_data->ceiling_color);
-	mlx_image_to_window(mlx, image, 0, 0);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
-
-
 	free_structs(main);
 	return (0);
 }
