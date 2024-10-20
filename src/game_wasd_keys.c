a12b80e484a20190ed7393fe9ac59b6571588405
Kai Klockow <kklockow@1-A-4.42heilbronn.de>
2024-10-20 03:59:15 +0200

    fixed movement now player is not longer able to go trough unfilled corners

--- src/game_wasd_keys.c
+++ src/game_wasd_keys.c
@@ -6,7 +6,7 @@
 /*   By: kklockow <kklockow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 00:44:12 by kklockow          #+#    #+#             */
-/*   Updated: 2024/10/20 00:53:17 by kklockow         ###   ########.fr       */
+/*   Updated: 2024/10/20 03:54:24 by kklockow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	w_key(t_main *main)
 {
-	int		x;
-	int		y;
+	t_point_int	current;
+	t_point_int	new;
 	float	distance_x;
 	float	distance_y;
 
@@ -23,9 +23,13 @@ void	w_key(t_main *main)
 	{
 		distance_y = main->player->delta.y * PLAYER_SPEED;
 		distance_x = main->player->delta.x * PLAYER_SPEED;
-		x = (int)(main->player->position.x + distance_x) >> 6;
-		y = (int)(main->player->position.y + distance_y) >> 6;
-		if (main->map_data->map_layout[y][x] != '1')
+		current.x = (int)(main->player->position.x) >> 6;
+		current.y = (int)(main->player->position.y) >> 6;
+		new.x = (int)(main->player->position.x + distance_x) >> 6;
+		new.y = (int)(main->player->position.y + distance_y) >> 6;
+		if (main->map_data->map_layout[new.y][new.x] != '1' &&
+			(main->map_data->map_layout[current.y][new.x] != '1'
+			|| main->map_data->map_layout[new.y][current.x] != '1'))
 		{
 			main->player->position.y += distance_y;
 			main->player->position.x += distance_x;
@@ -35,18 +39,22 @@ void	w_key(t_main *main)
 
 void	a_key(t_main *main)
 {
-	int		x;
-	int		y;
-	float	distance_x;
-	float	distance_y;
+	t_point_int	current;
+	t_point_int	new;
+	float		distance_x;
+	float		distance_y;
 
 	if (mlx_is_key_down(main->mlx, MLX_KEY_A))
 	{
 		distance_y = main->player->delta.y * PLAYER_SPEED;
 		distance_x = main->player->delta.x * PLAYER_SPEED;
-		x = (int)(main->player->position.x + distance_y) >> 6;
-		y = (int)(main->player->position.y - distance_x) >> 6;
-		if (main->map_data->map_layout[y][x] != '1')
+		current.x = (int)(main->player->position.x) >> 6;
+		current.y = (int)(main->player->position.y) >> 6;
+		new.x = (int)(main->player->position.x + distance_y) >> 6;
+		new.y = (int)(main->player->position.y - distance_x) >> 6;
+		if (main->map_data->map_layout[new.y][new.x] != '1' &&
+			(main->map_data->map_layout[current.y][new.x] != '1'
+			|| main->map_data->map_layout[new.y][current.x] != '1'))
 		{
 			main->player->position.y -= distance_x;
 			main->player->position.x += distance_y;
@@ -56,8 +64,8 @@ void	a_key(t_main *main)
 
 void	s_key(t_main *main)
 {
-	int		x;
-	int		y;
+	t_point_int	current;
+	t_point_int	new;
 	float	distance_x;
 	float	distance_y;
 
@@ -65,9 +73,13 @@ void	s_key(t_main *main)
 	{
 		distance_y = main->player->delta.y * PLAYER_SPEED;
 		distance_x = main->player->delta.x * PLAYER_SPEED;
-		x = (int)(main->player->position.x - distance_x) >> 6;
-		y = (int)(main->player->position.y - distance_y) >> 6;
-		if (main->map_data->map_layout[y][x] != '1')
+		current.x = (int)(main->player->position.x) >> 6;
+		current.y = (int)(main->player->position.y) >> 6;
+		new.x = (int)(main->player->position.x - distance_x) >> 6;
+		new.y = (int)(main->player->position.y - distance_y) >> 6;
+		if (main->map_data->map_layout[new.y][new.x] != '1' &&
+			(main->map_data->map_layout[current.y][new.x] != '1'
+			|| main->map_data->map_layout[new.y][current.x] != '1'))
 		{
 			main->player->position.y -= distance_y;
 			main->player->position.x -= distance_x;
@@ -77,8 +89,8 @@ void	s_key(t_main *main)
 
 void	d_key(t_main *main)
 {
-	int		x;
-	int		y;
+	t_point_int	current;
+	t_point_int	new;
 	float	distance_x;
 	float	distance_y;
 
@@ -86,9 +98,13 @@ void	d_key(t_main *main)
 	{
 		distance_y = main->player->delta.y * PLAYER_SPEED;
 		distance_x = main->player->delta.x * PLAYER_SPEED;
-		x = (int)(main->player->position.x - distance_y) >> 6;
-		y = (int)(main->player->position.y + distance_x) >> 6;
-		if (main->map_data->map_layout[y][x] != '1')
+		current.x = (int)(main->player->position.x) >> 6;
+		current.y = (int)(main->player->position.y) >> 6;
+		new.x = (int)(main->player->position.x - distance_y) >> 6;
+		new.y = (int)(main->player->position.y + distance_x) >> 6;
+		if (main->map_data->map_layout[new.y][new.x] != '1' &&
+			(main->map_data->map_layout[current.y][new.x] != '1'
+			|| main->map_data->map_layout[new.y][current.x] != '1'))
 		{
 			main->player->position.y += distance_x;
 			main->player->position.x -= distance_y;
