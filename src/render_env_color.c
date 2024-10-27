12d3967668765080e7cdbc0d5035dbbe77b5a0ea
Matthias-Strauss <matthias.strauss@proton.me>
2024-10-27 20:58:36 +0100

    figured out the math for colored env, LFG

--- src/render_env_color.c
+++ src/render_env_color.c
@@ -6,7 +6,7 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 15:54:38 by mstrauss          #+#    #+#             */
-/*   Updated: 2024/10/27 20:50:12 by mstrauss         ###   ########.fr       */
+/*   Updated: 2024/10/27 20:51:39 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,8 @@ void	draw_background_colored(t_main *main)
 	window_size = main->image->width * main->image->height;
 	px_dst = (uint32_t *)main->image->pixels;
 	px_src = (uint32_t *)(main->background->pixels + (main->image->height
-				- (main->mlx->height / 2)) * main->background->width * 4);
+				- (main->mlx->height / 2) - main->player->pitch)
+			* main->background->width * 4);
 	while (i < window_size)
 	{
 		px_dst[i] = px_src[i];
